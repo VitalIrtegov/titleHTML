1e01550769de2db21f16f149d34b0cf22e04099f
Vital <itpribor@mail.ru>
2022-08-06 14:18:30 +0500

    modified main

--- main.cpp
+++ main.cpp
@@ -5,8 +5,21 @@ int main() {
     std::cout << "Title HTML" << std::endl;
 
     std::string response, result;
-    cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/get"));
-    std::cout << r.text << std::endl;
+    std::string str1 = "<h1>";
+    std::string str2 = "</h1>";
+    cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/html"),
+                               cpr::Header({{"Accept", "text/html"}}));
+    response = r.text;
+
+    std::size_t index = response.find(str1) + str1.length();
+    result = response.substr(index);
+    index = result.find(str2);
+    result = result.substr(0, index);
+
+    std::cout << "result: " << result << std::endl;
+
+    //std::cout << "index1: " << index1 << std::endl;
+    //std::cout << "index2: " << index2 << std::endl;
 
     return 0;
 }
